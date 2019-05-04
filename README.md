60d43d287c4674251b9eb556cd3835a56c37da3e
nwjbrandon <nwjbrandon@gmail.com>
2019-05-04 23:07:04 +0700

    basic front end routing

--- README.md
+++ README.md
@@ -29,3 +29,6 @@ CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON * . * TO 'newuser'@'localhost';
 FLUSH PRIVILEGES;
 ```
+
+## References
+- https://codeburst.io/getting-started-with-react-router-5c978f70df91
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
+import App from './App'
+import Users from './users'
+import Contact from './contact'
+import Notfound from './notfound'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route exact path="/users" component={Users} />
+          <Route exact path="/contact" component={Contact} />
+          <Route component={Notfound} />
+        </Switch>
+      </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
