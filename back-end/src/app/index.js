61fa0c8bece52663a0c32063cbd4788f6d525c0d
nwjbrandon <nwjbrandon@gmail.com>
2019-05-04 20:57:57 +0700

    api calls working

--- back-end/src/app/index.js
+++ back-end/src/app/index.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const cors = require('cors')
-const sql = require('../db')
+// const sql = require('../db')
+const test = require('./test')
 const app = express()
 
 app.use(cors({credentials: true, origin: true}))
 
-app.get('/test', (req, res) => {
-  info = {'name': 'brandon'}
+/*
+ * example of basic api routing, refer to api.js to get the routing links
+ */
+app.use('/test', test) // type 'localhost:3001/test/hello' in browser
+app.get('/testing', (req, res) => { // type 'localhost:3001/testing' in browser
+  info = {'name': 'my_name'}
   res.send(info)
+  /*
+   * example of database call (do not delete)
+   *
   sql.query('select * from customers', function(err, rows, fields) {
     console.log(rows)
   })
+  */
 })
 
 module.exports = app
