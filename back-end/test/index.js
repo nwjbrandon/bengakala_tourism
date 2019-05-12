cd8f6e7dc6e0007cbf611dd91c24b210b105c5c7
nwjbrandon <nwjbrandon@gmail.com>
2019-05-12 18:53:51 +0700

    test coverage added

---
+++ back-end/test/index.js
@@ -0,0 +1,6 @@
+import { assert } from 'chai';  // Using Assert style
+import { expect } from 'chai';  // Using Expect style
+import { should } from 'chai';  // Using Should style
+import chai from 'chai';
+describe('List Information', () => {
+});
