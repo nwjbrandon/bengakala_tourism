a5ed2479832064773742ea089e0e5d2736d6de40
nwjbrandon <nwjbrandon@gmail.com>
2019-05-18 01:13:57 +0700

    configure backend to ignore redis for windows users

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import session from 'express-session';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import expressSwaggerGenerator from 'express-swagger-generator';
 import redis from 'redis';
 import connectRedis from 'connect-redis';
 import api from './api';
@@ -11,57 +10,41 @@ import db from './storage';
 
 const RedisStore = connectRedis(session);
 
-
-
-
 const app = express();
 const redisClient = redis.createClient();
-//const expressSwagger = expressSwaggerGenerator(app);
 
 
 
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./configuration/swagger')
-
-
 // use swagger-Ui-express for your app documentation endpoint
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-
-
-
-
-
-
-
 // api links
 app.use(bodyParser.json());
 app.use(cors({ credentials: true, origin: true }));
 app.use('/api', api);
-
-redisClient.on('error', (err) => {
-  console.log('Redis error:', err);
-});
-
-app.use(session({
-  secret: 'ThisIsHowYouUseRedisSessionStorage',
-  name: 'redisPractice',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }, // Note that the cookie-parser module is no longer needed
-  store: new RedisStore({ host: 'localhost', port: 6379, client: redisClient, ttl: 86400 }),
-}));
-
+if (process.platform === 'linux') {
+  console.log('Starting redis server');
+  redisClient.on('error', (err) => {
+    console.log('Redis error:', err);
+  });
+  app.use(session({
+    secret: 'ThisIsHowYouUseRedisSessionStorage',
+    name: 'redisPractice',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false}, // Note that the cookie-parser module is no longer needed
+    store: new RedisStore({host: 'localhost', port: 6379, client: redisClient, ttl: 86400}),
+  }));
+} else {
+  console.log('Note that redis cannot run on windows.');
+}
 const startExpress = (portNumber) => {
   app.listen(portNumber);
 };
 
 if (process.env.NODE_ENV === 'development') {
-  // development mode
-
-  // configurations for swagger docs
-  //expressSwagger(config.swaggerOptions);
   db.testing().then(r => console.log(r));
   startExpress(config.express.portNumber);
 } else {
