a5970b2bd3efb2a0d55efd054c517b14dc28f3b1
nwjbrandon <nwjbrandon@gmail.com>
2019-06-02 21:40:52 +0800

    not working edit buttons on dashboard/faq

--- front-end/src/routers/admin/dashboard/faq/index.js
+++ front-end/src/routers/admin/dashboard/faq/index.js
@@ -13,6 +13,26 @@ import Grid from '@material-ui/core/Grid';
 
 import NavBar from '../../../../components/dashboard/navBar';
 
+const tmp = [
+    {
+        title: 'Expansion Table 1',
+        type: 'General FAQ',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacusex, sit amet blandit leo lobortis eget.',
+        edit: false,
+        id: '1',
+        copy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacusex, sit amet blandit leo lobortis eget.',
+    },
+    {
+        title: 'Expansion Table 2',
+        type: 'Security FAQ',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacusex, sit amet blandit leo lobortis eget.',
+        edit: true,
+        id: '2',
+        copy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacusex, sit amet blandit leo lobortis eget.',
+    },
+]
+
+
 const styles = theme => ({
     root: {
         display: 'flex',
@@ -38,6 +58,28 @@ class DashboardFAQ extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            data: {
+                '1': {
+                    title: 'Expansion Table 1',
+                    type: 'General FAQ',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacusex, sit amet blandit leo lobortis eget.',
+                    edit: false,
+                    id: '1',
+                    copyTitle: 'Copy Expansion Table 1',
+                    copyType: 'Copy General FAQ',
+                    copyText: 'Copy Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacusex, sit amet blandit leo lobortis eget.',
+                },
+                '2': {
+                    title: 'Expansion Table 2',
+                    type: 'Security FAQ',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacusex, sit amet blandit leo lobortis eget.',
+                    edit: false,
+                    id: '2',
+                    copyTitle: 'Copy Expansion Table 1',
+                    copyType: 'Copy General FAQ',
+                    copyText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacusex, sit amet blandit leo lobortis eget.',
+                },
+            },
             title: 'FAQ',
             edit: false,
             question: 'Expansion Panel 1',
@@ -53,8 +95,11 @@ class DashboardFAQ extends Component {
         this.setState({edit: !this.state.edit })
     }
 
-    edit() {
-        this.setState({edit: !this.state.edit })
+    edit(val) {
+        const itemID = val.currentTarget.value
+        const newData = this.state.data
+        newData[itemID].edit = false
+        this.setState({ data: newData })
     }
 
     submit() {
@@ -65,13 +110,83 @@ class DashboardFAQ extends Component {
 
     render() {
         const { classes } = this.props
-        const { title, edit, text, question } = this.state
+        const { title, edit, text, question, data } = this.state
         return (
             <div className={classes.root}>
                 <CssBaseline />
                 <NavBar title={title} />
                 <main className={classes.content}>
                     <div className={classes.toolbar} />
+                    {Object.keys(data).map((item, index) => (
+                        <ExpansionPanel key={index}>
+                            <ExpansionPanelSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
+                            >
+                                <Typography className={classes.heading}>{ data[item].type }</Typography>
+                                &nbsp;-&nbsp;
+                                <Typography className={classes.heading}>{ data[item].title }</Typography>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                {data[item].edit ?
+                                    <Grid container alignItems="flex-start" justify="flex-start" direction="row">
+                                        <TextField
+                                            multiline={true}
+                                            variant="outlined"
+                                            fullWidth
+                                            value={data[item].copyTitle}
+                                            placeholder="Ex. Where are we located?"
+                                            label="Question"
+                                            className={classes.button}
+                                        />
+                                        <TextField
+                                            multiline={true}
+                                            variant="outlined"
+                                            fullWidth
+                                            value={data[item].copyType}
+                                            placeholder="Ex. General FAQ"
+                                            label="Type"
+                                            className={classes.button}
+                                        />
+                                        <TextField
+                                            multiline={true}
+                                            variant="outlined"
+                                            fullWidth
+                                            value={data[item].copyText}
+                                            placeholder="We are located at Bengkala, Indonesia"
+                                            label="Answer"
+                                            className={classes.button}
+                                        />
+                                    </Grid>
+                                    :
+                                    <Typography>
+                                        { data[item].type } - { data[item].title }
+                                    </Typography>
+                                }
+                            </ExpansionPanelDetails>
+                            { data[item].edit ?
+                                <Grid container alignItems="flex-start" justify="flex-end" direction="row">
+                                    <Button variant="contained" color="secondary" onClick={this.cancel} className={classes.button}>
+                                        Cancel
+                                    </Button>
+                                    <Button variant="contained" onClick={this.submit} className={classes.button}>
+                                        Submit
+                                    </Button>
+                                </Grid>
+                                :
+                                <Grid container alignItems="flex-start" justify="flex-end" direction="row">
+                                    <Button variant="contained" value={data[item].id} onClick={this.edit} className={classes.button}>
+                                        Edit
+                                    </Button>
+                                    <Button variant="contained" color="secondary" onClick={this.delete} className={classes.button}>
+                                        Delete
+                                    </Button>
+                                </Grid>
+                            }
+                        </ExpansionPanel>
+
+                    ))}
                     <ExpansionPanel>
                         <ExpansionPanelSummary
                             expandIcon={<ExpandMoreIcon />}
