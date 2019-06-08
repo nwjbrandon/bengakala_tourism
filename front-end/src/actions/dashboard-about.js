7cc6192d4e1a4d5df59052a105238e7424e7e8e2
nwjbrandon <nwjbrandon@gmail.com>
2019-06-08 22:18:35 +0800

    completed saga and redux for dashboard-about

--- front-end/src/actions/dashboard-about.js
+++ front-end/src/actions/dashboard-about.js
@@ -1,31 +1,13 @@
-import { createAction, handleActions } from 'redux-actions';
+import { createAction } from 'redux-actions';
 
-export const DASHBOARD_ABOUT_INSERT = (payload) => {
-    return {
-        type: 'DASHBOARD_ABOUT_INSERT',
-        payload
-    }
-};
+export const DASHBOARD_ABOUT_INSERT_NAME = 'DASHBOARD_ABOUT_INSERT';
+export const DASHBOARD_ABOUT_INSERT = createAction(DASHBOARD_ABOUT_INSERT_NAME);
 
-export const DASHBOARD_ABOUT_RESET = () => {
-    return {
-        type: 'DASHBOARD_ABOUT_RESET',
-    }
-};
+export const DASHBOARD_ABOUT_RESET_NAME = 'DASHBOARD_ABOUT_RESET';
+export const DASHBOARD_ABOUT_RESET = createAction(DASHBOARD_ABOUT_RESET_NAME);
 
-export const DASHBOARD_ABOUT_UPDATE = (payload) => {
-    return {
-        type: 'DASHBOARD_ABOUT_UPDATE',
-        payload
-    }
-};
-/*
-export const DASHBOARD_ABOUT_ONMOUNT = () => {
-    return {
-        type: 'DASHBOARD_ABOUT_ONMOUNT',
-    }
-};
-*/
+export const DASHBOARD_ABOUT_UPDATE_NAME = 'DASHBOARD_ABOUT_UPDATE';
+export const DASHBOARD_ABOUT_UPDATE = createAction(DASHBOARD_ABOUT_UPDATE_NAME);
 
 export const DASHBOARD_ABOUT_ONMOUNT_NAME = 'DASHBOARD_ABOUT_ONMOUNT';
 export const DASHBOARD_ABOUT_ONMOUNT = createAction(DASHBOARD_ABOUT_ONMOUNT_NAME);
--- front-end/src/reducers/index.js
+++ front-end/src/reducers/index.js
@@ -2,13 +2,13 @@ import {combineReducers} from 'redux';
 import reduceReducers from 'reduce-reducers';
 import auth from './auth';
 import statements from './statements'
-import { dashboardAboutReducer, dashboardAboutReducerSaga } from './dashboard-about'
+import { dashboardAboutReducer } from './dashboard-about-onmount'
 
 const allReducers = combineReducers({
     admin: reduceReducers(statements, auth),
     statements,
     auth,
-    dashboardAbout: reduceReducers(dashboardAboutReducer, dashboardAboutReducerSaga),
+    dashboardAbout: reduceReducers(dashboardAboutReducer),
 });
 
 export default allReducers
