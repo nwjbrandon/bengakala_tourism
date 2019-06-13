2252802d7b71bf91d9e03bb8e7f7966e2ee03dc9
nwjbrandon <nwjbrandon@gmail.com>
2019-06-13 20:20:08 +0700

    contact page done

--- back-end/src/api/contact/index.js
+++ back-end/src/api/contact/index.js
@@ -1,24 +1,21 @@
+import _ from 'lodash';
+import db from '../../storage/db';
+import { TABLE_INFORMATION } from '../../storage/tableName';
 
-const express = require('express');
-const app = express();
-
-/**
- * This function comment is parsed by doctrine testing
- * @route GET /api
- * @group foo - Operations about user
- * @param {string} email.query.required - username or email - eg: user@domain
- * @param {string} password.query.required - user's password.
- * @returns {object} 200 - An array of user info
- * @returns {Error}  default - Unexpected error
- */
-const getContact = [
-  async(req, res) => {
-    const info = { data: 'myContact' };
-    res.send(info);
+const contactInfo = [
+  async (req, res) => {
+    const contacts = await db.fetchData(TABLE_INFORMATION, { type: 'contact' });
+    const info = _.map(contacts, contact => (
+      {
+        title: contact.title,
+        info: contact.text,
+      }));
+    res.json({
+      data: info,
+    });
   },
 ];
 
 export default {
-  get: getContact,
+  info: contactInfo,
 };
-
