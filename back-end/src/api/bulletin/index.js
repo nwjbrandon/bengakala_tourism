6aefaeec3015218010e30062937f7b4423089614
nwjbrandon <nwjbrandon@gmail.com>
2019-07-10 00:51:11 +0800

    added the date into the bulletin

--- back-end/src/api/bulletin/index.js
+++ back-end/src/api/bulletin/index.js
@@ -5,12 +5,13 @@ import { TABLE_INFORMATION } from '../../storage/tableName';
 const bulletinInfo = [
   async (req, res) => {
     const bulletins = await db.fetchData(TABLE_INFORMATION, { type: 'media' });
-    const data = _.map(bulletins, bulletin => ({
+    const data = _.orderBy(_.map(bulletins, bulletin => ({
       title: bulletin.title,
       imgUrl: bulletin.imgUrl,
       text: bulletin.text,
       summary: bulletin.heading,
-    }));
+      createdAt: bulletin.createdAt,
+    })), ['createdAt'], ['desc']);
     res.json({
       data
     });
