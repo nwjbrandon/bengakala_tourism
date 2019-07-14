2a9db36700b2a018b3e135044d3876b1aa8b4088
nwjbrandon <nwjbrandon@gmail.com>
2019-07-14 16:50:03 +0800

    redesign the contact page

--- front-end/src/routers/home/index.js
+++ front-end/src/routers/home/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Background from '../../components/home/background'
 import Content from '../../components/home/content.container'
-import Navbar from "../../components/navBar/navbar";
 import SuccessToast from "../../components/snackBar/successSnackBar.container";
 import ErrorToast from "../../components/snackBar/errorSnackBar.container";
 
