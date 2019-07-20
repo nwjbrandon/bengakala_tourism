8556d611c73549e6ff4c479e15fc625b3b4a931c
nwjbrandon <nwjbrandon@gmail.com>
2019-07-20 22:15:34 +0800

    extra comments on db

--- back-end/src/storage/tableName.js
+++ back-end/src/storage/tableName.js
@@ -1,3 +1,4 @@
+// note that this database design is terrible
 export const TABLE_ADMINISTRATOR = 'ADMINISTRATOR';
 export const TABLE_INFORMATION = 'INFORMATION';
 export const TABLE_TRANSACTIONS = 'TRANSACTIONS';
@@ -5,7 +6,7 @@ export const TABLE_EXCLUDED_DATES = 'EXCLUDED_DATES';
 /*
  * Data: Transactions
  * Table Name: TRANSACTIONS
- * uuid: string - midtrans id
+ * uuid: string - mid trans id
  * firstName: string - first name
  * lastName: string - last name
  * email: string - email
@@ -26,37 +27,58 @@ export const TABLE_EXCLUDED_DATES = 'EXCLUDED_DATES';
  */
 
 /*
- * Data: Stories
+ * Data: Administrators
  * Table Name: ADMINISTRATOR
- *
+ * uuid: entry id
+ * username: email address
+ * email: email address
+ * password: hash of the password
+ * phone: contact number
+ * jobTitle job title
+ * createdAt: time of account creation
+ * edit: is root admin
  */
 
 /*
- * Data: Transactions
- * Table Name: ADMINISTRATOR
- *
+ * Data: Excluded dates
+ * Table Name: EXCLUDED_DATES
+ * uuid: string - entry id
+ * date: datetime - date to exclude
+ * value: string - always -1
+ * createdAt: timestamp - entry created
  */
 
 /*
- * Data: Transactions
- * Table Name: ADMINISTRATOR
- *
+ * Data: Cost
+ * Table Name: INFORMATION
+ * uuid: string - entry id
+ * title: string - item name
+ * pricesString: string - item cost
+ * type: string - entry type
+ * edit: boolean - always 0
+ * createdAt: timestamp - entry created
  */
 
 /*
- * Data: Transactions
- * Table Name: ADMINISTRATOR
- *
+ * Data: Accommodation Pictures
+ * Table Name: INFORMATION
+ * uuid: string - entry id
+ * title: string - accommodation description
+ * imgUrl: string - image links
+ * type: string - entry type
+ * edit: boolean - always 0
+ * createdAt: timestamp - entry created
  */
 
 /*
- * Data: Transactions
- * Table Name: ADMINISTRATOR
- *
+ * Data: Contact Details
+ * Table Name: INFORMATION
+ * uuid: string - entry id
+ * title: string - title of the detail
+ * text: string - information of the detail
+ * type: string - entry type
+ * edit: boolean - always 0
+ * createdAt: timestamp - entry created
  */
 
-/*
- * Data: Transactions
- * Table Name: ADMINISTRATOR
- *
- */
+// My computer battery died, so I gave up commenting. But it is quite obvious honestly.
