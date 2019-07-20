dd88c74a442256c6e237deb20abe9f28e274d788
nwjbrandon <nwjbrandon@gmail.com>
2019-07-21 01:08:49 +0800

    fixed some linting issues with the notifications api

--- back-end/src/api/notification/index.js
+++ back-end/src/api/notification/index.js
@@ -2,13 +2,12 @@ import midtransClient from 'midtrans-client';
 import { serverKey, clientKey } from '../../secret/midtransSecret';
 import { wrapAsync } from '../../middleware/errorHandling';
 import db from '../../storage/db';
+import { TABLE_TRANSACTIONS } from '../../storage/tableName';
 
 // getTransactionStatus();
 const updateDB = async (orderID, paymentStat) => {
-
   await db.updateData(TABLE_TRANSACTIONS, { cash: paymentStat }, { uuid: orderID });
-
-}
+};
 
 
 const notificationPost = [
@@ -26,39 +25,36 @@ const notificationPost = [
     const orderId = statusResponse.order_id;
     const transactionStatus = statusResponse.transaction_status;
     const fraudStatus = statusResponse.fraud_status;
-    const transactionId = statusResponse.transaction_id
+    const transactionId = statusResponse.transaction_id;
 
     console.log(`Transaction notification received. Order ID: ${orderId}. Transaction status: ${transactionStatus}. Fraud status: ${fraudStatus}`);
 
     const UUIDexists = await db.uuidExist(TABLE_TRANSACTIONS, transactionId);
 
-    const Data = await db.fetchData(TABLE_TRANSACTIONS, { uuid: transactionId })
+    const Data = await db.fetchData(TABLE_TRANSACTIONS, { uuid: transactionId });
 
-    console.log(Data)
+    console.log(Data);
 
     if (UUIDexists) {
-
       if (transactionStatus === 'capture') {
         if (fraudStatus === 'challenge') {
-          // TODO set transaction status on databaase to 'challenge'
-          updateDB(transactionId, 1);
+          // TODO set transaction status on database to 'challenge'
+          await updateDB(transactionId, 1);
         } else if (fraudStatus === 'accept') {
-          updateDB(transactionId, 2);
-          // TODO set transaction status on databaase to 'success' and send email here
+          await updateDB(transactionId, 2);
+          // TODO set transaction status on database to 'success' and send email here
         }
       } else if (transactionStatus === 'cancel'
         || transactionStatus === 'deny'
         || transactionStatus === 'expire') {
         // do nothing
       } else if (transactionStatus === 'pending') {
-        updateDB(transactionId, 1);
-        // TODO set transaction status on your databaase to 'pending' / waiting payment
+        await updateDB(transactionId, 1);
+        // TODO set transaction status on your database to 'pending' / waiting payment
       }
-
     }
-
+    return res.json({ data: 'success' });
   })
-
 ];
 export default {
   post: notificationPost
--- back-end/src/api/updateTransactionState/index.js
+++ back-end/src/api/updateTransactionState/index.js
@@ -1,128 +1,115 @@
 import db from '../../storage/db';
-import { TABLE_EXCLUDED_DATES, TABLE_INFORMATION, TABLE_TRANSACTIONS } from '../../storage/tableName';
-import { eachDay } from 'date-fns';
-import calculations from '../../middleware/calculations'
+import { TABLE_INFORMATION, TABLE_TRANSACTIONS } from '../../storage/tableName';
+import calculations from '../../middleware/calculations';
 
-import { serverKey, clientKey } from '../../secret/midtransSecret';
-import sendEmail from '../../utils/emailSender/emailSender'
+import { clientKey, serverKey } from '../../secret/midtransSecret';
+import sendEmail from '../../utils/emailSender/emailSender';
 
 const midtransClient = require('midtrans-client');
 
 const apiClient = new midtransClient.Snap({
-    isProduction: false,
-    serverKey: serverKey,
-    clientKey: clientKey
+  isProduction: false,
+  serverKey,
+  clientKey
 });
 
 const updateDB = async (orderID, paymentStat) => {
-
-    await db.updateData(TABLE_TRANSACTIONS, { cash: paymentStat }, { uuid: orderID });
-
-}
+  await db.updateData(TABLE_TRANSACTIONS, { cash: paymentStat }, { uuid: orderID });
+};
 
 const constructStringDate = (date) => {
-    const DateObj = date ? new Date(date) : new Date();
-    const str = `${DateObj.getFullYear()}-${DateObj.getMonth() + 1}-${DateObj.getDate()}`
-    console.log(str)
-    return str
-}
+  const DateObj = date ? new Date(date) : new Date();
+  const str = `${DateObj.getFullYear()}-${DateObj.getMonth() + 1}-${DateObj.getDate()}`;
+  console.log(str);
+  return str;
+};
 
 
-const refractorOrder = (myOrder) => {
+const refractorOrder = myOrder => ({
 
-    const refractoredOrder = {
-
-        firstName: myOrder.firstName,
-        lastName: myOrder.lastName,
-        email: myOrder.email,
-        country: myOrder.country,
+  firstName: myOrder.firstName,
+  lastName: myOrder.lastName,
+  email: myOrder.email,
+  country: myOrder.country,
 
 
-        checkIn: myOrder.dateFrom,
-        checkOut: myOrder.dateTo,
-        breakfast: myOrder.breakfast,
-        lunch: myOrder.lunch,
-        dinner: myOrder.dinner,
-        numberMales: parseInt(myOrder.males, 10),
-        numberFemales: parseInt(myOrder.females, 10),
-        numberVans: parseInt(myOrder.van, 10),
-        numberCars: parseInt(myOrder.cars, 10),
-        numberBikes: parseInt(myOrder.motorbikes, 10),
+  checkIn: myOrder.dateFrom,
+  checkOut: myOrder.dateTo,
+  breakfast: myOrder.breakfast,
+  lunch: myOrder.lunch,
+  dinner: myOrder.dinner,
+  numberMales: parseInt(myOrder.males, 10),
+  numberFemales: parseInt(myOrder.females, 10),
+  numberVans: parseInt(myOrder.van, 10),
+  numberCars: parseInt(myOrder.cars, 10),
+  numberBikes: parseInt(myOrder.motorbikes, 10),
 
-        transactionID: myOrder.uuid
-    }
-
-    return refractoredOrder
-}
+  transactionID: myOrder.uuid
+});
 
 const updateState = [
-    async (req, res) => {
+  async (req, res) => {
+    const transactionID = req.body.data.uuid;
+    console.log('Transaction!!!', transactionID);
 
-        const transactionID = req.body.data.uuid;
-        console.log("Transaction!!!", transactionID);
+    const UUIDexists = await db.uuidExist(TABLE_TRANSACTIONS, transactionID);
 
-        const UUIDexists = await db.uuidExist(TABLE_TRANSACTIONS, transactionID);
+    if (UUIDexists) {
+      try {
+        const response = await apiClient.transaction.status(transactionID);
 
-        if (UUIDexists) {
-            try {
-                const response = await apiClient.transaction.status(transactionID);
+        if (response.fraud_status === 'accept') {
+          const services = await db.fetchData(TABLE_INFORMATION, { type: 'cost' });
+          console.log('SERVICES', services);
 
-                if (response.fraud_status === 'accept') {
+          const Order = await db.fetchData(TABLE_TRANSACTIONS, { uuid: transactionID });
+          const myOrder = Order[0];
+          console.log('myOrder', myOrder);
 
-                    const services = await db.fetchData(TABLE_INFORMATION, { type: 'cost' });
-                    console.log("SERVICES", services);
+          const CalculationData = new Object();
 
-                    const Order = await db.fetchData(TABLE_TRANSACTIONS, { uuid: transactionID });
-                    const myOrder = Order[0];
-                    console.log("myOrder", myOrder);
+          CalculationData.tripDetails = { ...refractorOrder(myOrder) };
 
-                    const CalculationData = new Object();
+          CalculationData.cost = new Object();
 
-                    CalculationData.tripDetails = { ...refractorOrder(myOrder) }
+          services.forEach((item) => {
+            const price = parseInt(item.pricesString, 10);
+            CalculationData.cost[item.title.toLowerCase()] = price;
+          });
 
-                    CalculationData.cost = new Object();
+          console.log('CalculationData.cost', CalculationData.cost);
+          const { prices, numberOfDays } = calculations(CalculationData);
+          console.log({ prices, numberOfDays });
+          const EmailData = {
+            tripDetails: CalculationData.tripDetails,
+            cost: CalculationData.cost,
+            prices,
+            numberOfDays,
+            orderStatus: myOrder.cash,
+            transactionID: myOrder.uuid,
+            toEmail: myOrder.email,
+            checkIn: constructStringDate(CalculationData.tripDetails.checkIn),
+            checkOut: constructStringDate(CalculationData.tripDetails.checkOut),
+            Now: constructStringDate(),
+          };
 
-                    services.forEach((item) => {
-                        const price = parseInt(item.pricesString, 10);
-                        CalculationData.cost[item.title.toLowerCase()] = price;
-                    })
-
-                    console.log("CalculationData.cost", CalculationData.cost)
-                    const { prices, numberOfDays } = calculations(CalculationData);
-                    console.log({ prices, numberOfDays })
-                    const EmailData = {
-                        tripDetails: CalculationData.tripDetails,
-                        cost: CalculationData.cost,
-                        prices: prices,
-                        numberOfDays: numberOfDays,
-                        orderStatus: myOrder.cash,
-                        transactionID: myOrder.uuid,
-                        toEmail: myOrder.email,
-                        checkIn: constructStringDate(CalculationData.tripDetails.checkIn),
-                        checkOut: constructStringDate(CalculationData.tripDetails.checkOut),
-                        Now: constructStringDate(),
-                    }
-
-                    await sendEmail(EmailData);
-                    await updateDB(transactionID, 2)
-                }
-
-            } catch (err) {
-                console.log(err)
-            }
-
-            res.json({
-                data: 'success',
-            });
-        } else {
-            res.json({
-                data: 'not found',
-            });
+          await sendEmail(EmailData);
+          await updateDB(transactionID, 2);
         }
+      } catch (err) {
+        console.log(err);
+      }
 
-
+      res.json({
+        data: 'success',
+      });
+    } else {
+      res.json({
+        data: 'not found',
+      });
     }
+  }
 ];
 export default {
-    update: updateState
+  update: updateState
 };
